ee468b16449792b8f03801db56ed36831eb488ed
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-08-11 17:21:59 +0300

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-  .addEntry('main', './assets/js/app.ts')
+  .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -38,10 +38,7 @@ Encore
     .enableSassLoader()
 
     // uncomment if you use TypeScript
-    .enableTypeScriptLoader(function (typeScriptConfigOptions) {
-      typeScriptConfigOptions.transpileOnly = true;
-      typeScriptConfigOptions.configFile = '/tsconfig.json';
-    })
+    // .enableTypeScriptLoader()
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
